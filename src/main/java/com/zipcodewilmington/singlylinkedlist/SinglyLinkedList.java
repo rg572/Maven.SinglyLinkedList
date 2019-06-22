1dacf4f9506057985a9de5d504c3aab559b596e0
robertgordon <robertgordon.subscript2@gmail.com>
2019-06-21 21:28:47 -0400

    all required methods added and passing tests

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -3,17 +3,18 @@ package com.zipcodewilmington.singlylinkedlist;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList<T> {
+public class SinglyLinkedList<T extends Comparable<T>> {
 
-    SinglyLinkedListNode<T> firstNode;
-    Integer size;
+    private SinglyLinkedListNode<T> firstNode;
+    private Integer size;
 
     public SinglyLinkedList(){
         this.firstNode = null;
         this.size = 0;
     }
 
-    public void add(SinglyLinkedListNode<T> node){
+    public void add(T o){
+        SinglyLinkedListNode<T> node = new SinglyLinkedListNode<>(o);
         if(firstNode == null){
             firstNode = node;
         }
@@ -26,6 +27,44 @@ public class SinglyLinkedList<T> {
         }
         size++;
     }
+
+    public void remove(Integer index){
+        if(index >= size || index < 0){
+            throw new IndexOutOfBoundsException();
+        }
+        SinglyLinkedListNode<T> node = firstNode;
+        SinglyLinkedListNode<T> prevNode = firstNode;
+
+        Integer count = 1;
+        while(count <= index){
+            node = node.getNextNode();
+
+            if(count == index){
+                prevNode.setNextNode(node.getNextNode());
+                size--;
+                return;
+            }
+            prevNode = node;
+            count++;
+        }
+
+    }
+
+    public Boolean contains(T o){
+        return find(o) != -1;
+    }
+
+    public Integer find(T o){
+        SinglyLinkedListNode<T> node = firstNode;
+        for(int i = 0; i < size; i++){
+            if(o.equals(node.getStoredObject())){
+                return i;
+            }
+            node = node.getNextNode();
+        }
+        return -1;
+    }
+
 
     public Integer size(){
         return size;
@@ -47,5 +86,59 @@ public class SinglyLinkedList<T> {
             }
         }
         throw new IndexOutOfBoundsException("This should never have been reached");
+    }
+
+
+    public void sort(){
+        if(size < 2){
+            return;
+        }
+
+        SinglyLinkedListNode<T> keepTrackNode = new SinglyLinkedListNode<>(null);
+        keepTrackNode.setNextNode(firstNode);
+
+        SinglyLinkedListNode<T> prevNode = keepTrackNode;
+        SinglyLinkedListNode<T> thisNode = firstNode;
+        SinglyLinkedListNode<T> nextNode = firstNode.getNextNode();
+
+        for(int i = 0; i < size - 1; i++){
+            Boolean swapped = false;
+            for(int j = 0; j < size() - i -1; j++){
+                if(thisNode.getStoredObject().compareTo(nextNode.getStoredObject()) > 0){
+                    swapped = true;
+                    thisNode.setNextNode(nextNode.getNextNode());
+                    nextNode.setNextNode(thisNode);
+                    prevNode.setNextNode(nextNode);
+
+                    prevNode = nextNode;
+                    nextNode = thisNode.getNextNode();
+                }
+                else {
+                    nextNode = nextNode.getNextNode();
+                    thisNode = thisNode.getNextNode();
+                    prevNode = prevNode.getNextNode();
+                }
+            }
+            if(!swapped){
+                break;
+            }
+            prevNode = keepTrackNode;
+            thisNode = prevNode.getNextNode();
+            nextNode = thisNode.getNextNode();
+        }
+        firstNode = keepTrackNode.getNextNode();
+
+    }
+
+    public SinglyLinkedList<T> copy(){
+        SinglyLinkedList<T> list = new SinglyLinkedList<>();
+
+        SinglyLinkedListNode<T> node = firstNode;
+
+        while(node != null){
+            list.add(node.getStoredObject());
+            node = node.getNextNode();
+        }
+        return list;
     }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -11,15 +11,14 @@ public class SinglyLinkedListTest {
     @Test
     public void addTest1(){
         // Arrange
+        String str1 = "Grr";
         String expected = "Arg";
         Integer expectedSize = 2;
         SinglyLinkedList<String> list = new SinglyLinkedList<>();
-        SinglyLinkedListNode<String> node1 = new SinglyLinkedListNode<>("Grr");
-        SinglyLinkedListNode<String> node2 = new SinglyLinkedListNode<>(expected);
 
         // Act
-        list.add(node1);
-        list.add(node2);
+        list.add(str1);
+        list.add(expected);
         String actual = list.get(1);
         Integer actualSize = list.size();
 
@@ -32,12 +31,12 @@ public class SinglyLinkedListTest {
     public void addTest2(){
         // Arrange
         SinglyLinkedList<String> list = new SinglyLinkedList<>();
-        SinglyLinkedListNode<String> node1 = new SinglyLinkedListNode<>("Grr");
-        SinglyLinkedListNode<String> node2 = new SinglyLinkedListNode<>("Arg");
+        String str1 = "Grr";
+        String str2 = "Arg";
 
         // Act
-        list.add(node1);
-        list.add(node2);
+        list.add(str1);
+        list.add(str2);
         String actual = list.get(2);
 
     }
@@ -46,13 +45,251 @@ public class SinglyLinkedListTest {
     public void addTest3(){
         // Arrange
         SinglyLinkedList<String> list = new SinglyLinkedList<>();
-        SinglyLinkedListNode<String> node1 = new SinglyLinkedListNode<>("Grr");
-        SinglyLinkedListNode<String> node2 = new SinglyLinkedListNode<>("Arg");
+        String str1 = "Grr";
+        String str2 = "Arg";
 
         // Act
-        list.add(node1);
-        list.add(node2);
+        list.add(str1);
+        list.add(str2);
         String actual = list.get(-2);
 
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void removeTest1(){
+        // Arrange
+        SinglyLinkedList<String> list = new SinglyLinkedList<>();
+        String str1 = "Grr";
+        String str2 = "Arg";
+
+        // Act
+        list.add(str1);
+        list.add(str2);
+        list.remove(-2);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void removeTest2(){
+        // Arrange
+        SinglyLinkedList<String> list = new SinglyLinkedList<>();
+        String str1 = "Grr";
+        String str2 = "Arg";
+
+        // Act
+        list.add(str1);
+        list.add(str2);
+        list.remove(3);
+    }
+
+    @Test
+    public void removeTest3(){
+        // Arrange
+        SinglyLinkedList<String> list = new SinglyLinkedList<>();
+        String str1 = "Grr";
+        String str2 = "Arg";
+        String str3 = "Mutant";
+        String str4 = "Enemy";
+        Integer expectedLength = 3;
+        String expectedValue = "Enemy";
+
+
+        // Act
+        list.add(str1);
+        list.add(str2);
+        list.add(str3);
+        list.add(str4);
+        list.remove(1);
+        Integer actualLength = list.size();
+        String actualValue = list.get(2);
+
+        // Assert
+        Assert.assertEquals(expectedLength, actualLength);
+        Assert.assertEquals(expectedValue, actualValue);
+
+    }
+
+    @Test
+    public void removeTest4(){
+        // Arrange
+        SinglyLinkedList<String> list = new SinglyLinkedList<>();
+        String str1 = "Grr";
+        String str2 = "Arg";
+        String str3 = "Mutant";
+        String str4 = "Enemy";
+        Integer expectedLength = 3;
+        String expectedValue = "Mutant";
+
+
+        // Act
+        list.add(str1);
+        list.add(str2);
+        list.add(str3);
+        list.add(str4);
+        list.remove(3);
+        Integer actualLength = list.size();
+        String actualValue = list.get(2);
+
+        // Assert
+        Assert.assertEquals(expectedLength, actualLength);
+        Assert.assertEquals(expectedValue, actualValue);
+
+    }
+
+    @Test
+    public void sortTest1(){
+        // Arrange;
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        Integer e1 = 1;
+        Integer e2 = 2;
+        Integer e3 = 3;
+        Integer e4 = 4;
+        Integer e5 = 5;
+        list.add(e5);
+        list.add(e4);
+        list.add(e3);
+        list.add(e2);
+        list.add(e1);
+
+        // Act
+        list.sort();
+        Integer a1 = list.get(0);
+        Integer a2 = list.get(1);
+        Integer a3 = list.get(2);
+        Integer a4 = list.get(3);
+        Integer a5 = list.get(4);
+
+        // Assert
+        Assert.assertEquals(e1, a1);
+        Assert.assertEquals(e2, a2);
+        Assert.assertEquals(e3, a3);
+        Assert.assertEquals(e4, a4);
+        Assert.assertEquals(e5, a5);
+
+    }
+
+    @Test
+    public void sortTest2(){
+        // Arrange
+        SinglyLinkedList<String> list = new SinglyLinkedList<>();
+        list.add("Eat");
+        list.add("Shit");
+        list.add("And");
+        list.add("Die");
+
+        // Act
+        list.sort();
+        String a1 = list.get(0);
+        String a2 = list.get(1);
+        String a3 = list.get(2);
+        String a4 = list.get(3);
+
+        // Assert
+        Assert.assertEquals("And", a1);
+        Assert.assertEquals("Die", a2);
+        Assert.assertEquals("Eat", a3);
+        Assert.assertEquals("Shit",a4);
+
+    }
+
+    @Test
+    public void containsTest1(){
+        // Arrange
+        SinglyLinkedList<String> list = new SinglyLinkedList<String>();
+        list.add("Grr");
+        list.add("Arg");
+        list.add("Mutant");
+        list.add("Enemy");
+
+        // Act
+        Boolean actual = list.contains("Mutant");
+
+        // Assert
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void containsTest2(){
+        // Arrange
+        SinglyLinkedList<String> list = new SinglyLinkedList<String>();
+        list.add("Grr");
+        list.add("Arg");
+        list.add("Mutant");
+        list.add("Enemy");
+
+        // Act
+        Boolean actual = list.contains("Entertainment");
+
+        // Assert
+        Assert.assertFalse(actual);
+    }
+
+    @Test
+    public void findTest1(){
+        // Arrange
+        SinglyLinkedList<String> list = new SinglyLinkedList<String>();
+        list.add("Grr");
+        list.add("Arg");
+        list.add("Mutant");
+        list.add("Enemy");
+        Integer expected = 2;
+
+        // Act
+        Integer actual = list.find("Mutant");
+
+        // Assert
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findTest2(){
+        // Arrange
+        SinglyLinkedList<String> list = new SinglyLinkedList<String>();
+        list.add("Grr");
+        list.add("Arg");
+        list.add("Mutant");
+        list.add("Enemy");
+        Integer expected = -1;
+
+        // Act
+        Integer actual = list.find("Entertainment");
+
+        // Assert
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void copyTest1(){
+        // Arrange
+        SinglyLinkedList<String> list = new SinglyLinkedList<>();
+        list.add("Grr");
+        list.add("Arg");
+        list.add("Mutant");
+        list.add("Enemy");
+        Integer expected = 4;
+
+        // Act
+        SinglyLinkedList<String> list2 = list.copy();
+        Integer actual = list2.size();
+
+        // Assert
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void copyTest2(){
+        // Arrange
+        SinglyLinkedList<String> list = new SinglyLinkedList<>();
+        list.add("Grr");
+        list.add("Arg");
+        list.add("Mutant");
+        list.add("Enemy");
+        String expected = "Mutant";
+
+        // Act
+        SinglyLinkedList<String> list2 = list.copy();
+        String actual = list2.get(2);
+
+        // Assert
+        Assert.assertEquals(expected, actual);
     }
 }
